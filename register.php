60f29ce235ac6fa4fb1638784a526348a55d4442
donkey17191 <dummy17191@gmail.com>
2020-08-10 22:14:54 +0700

    button press echo

--- register.php
+++ register.php
@@ -1,3 +1,19 @@
+<?php
+
+if (isset($_POST['loginButton'])) {
+    echo "login button was pressed";
+//    login button was pressed
+}
+
+if (isset($_POST['registerButton'])) {
+    echo "register button was pressed";
+//    register button was pressed
+}
+
+
+
+?>
+
 <html>
 <head>
 	<title>Welcome to Slotify!</title>
@@ -13,12 +29,57 @@
 			</p>
 			<p>
 				<label for="loginPassword">Password</label>
-				<input id="loginPassword" name="loginPassword" type="password" required>
+				<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
 			</p>
 
 			<button type="submit" name="loginButton">LOG IN</button>
 			
 		</form>
+
+
+
+		<form id="registerForm" action="register.php" method="POST">
+			<h2>Create your free account</h2>
+			<p>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
+
+			<p>
+				<label for="firstName">First name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+			</p>
+
+			<p>
+				<label for="lastName">Last name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+			</p>
+
+			<p>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
+			</p>
+
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" required>
+			</p>
+
+			<p>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" placeholder="Your password" required>
+			</p>
+
+			<p>
+				<label for="password2">Confirm password</label>
+				<input id="password2" name="password2" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="registerButton">SIGN UP</button>
+			
+		</form>
+
+
 	</div>
 
 </body>
