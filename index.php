71e2c9800be0a92539a3f26430d88c42827444c8
donkey17191 <dummy17191@gmail.com>
2020-08-10 09:07:24 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Slotify</title>
+</head>
+<body>
+    <div id="inputContainer">
+        <form id="loginForm"  action="register.php" method="POST">
+            <h2>Login to your account</h2>
+            <p>
+                <input id="loginUserName" name="loginUserName" type="text" placeholder="e.g. bartSimson" required>
+            </p>
+            <p>
+                <input id="loginPassword" name="loginPassword" type="password" required>
+            </p>
+
+
+        </form>
+
+    </div>
+
+</body>
+</html>
