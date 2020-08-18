14851ad1613b3de58506649b1e0b11f321166f62
donkey17191 <donkey17191@gmail.com>
2020-08-18 21:03:22 +0700

    suggestion complete

--- suggest.php
+++ suggest.php
@@ -29,3 +29,22 @@ $people[] = "Mike";
 // Get Query String
 
 $q = $_REQUEST['q'];
+
+$suggestion = "";
+
+// Get Suggestion
+if ($q !== "") {
+    $q = strtolower($q);
+    $len = strlen($q);
+    foreach ($people as $person) {
+        if (stristr($q, substr($person, 0, $len))) {
+            if ($suggestion === "") {
+                $suggestion = $person;
+            } else {
+                $suggestion .= ", $person";
+            }
+        }
+    }
+}
+
+echo $suggestion === "" ? "No Suggestion" : $suggestion;
