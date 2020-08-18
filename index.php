a63103c0fbceac34695a497fb9142dff01161a15
donkey17191 <donkey17191@gmail.com>
2020-08-18 13:46:29 +0700

    still remains show data

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $msg = '';
 $msgClass = '';
     // check for submit
 if (filter_has_var(INPUT_POST, 'submit')) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
     // check required fields
 
@@ -52,15 +52,15 @@ if (filter_has_var(INPUT_POST, 'submit')) {
         <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" name="name" class="form-control" value="">
+                <input type="text" name="name" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
             </div>
             <div class="form-group">
                 <label>Email</label>
-                <input type="text" name="email" class="form-control" value="">
+                <input type="text" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
             </div>
             <div class="form-group">
                 <label>Message</label>
-                <textarea name="message" class="form-control"></textarea>
+                <textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
             </div>
             <br>
             <button type="submit" name="submit" class="btn btn-primary">Submit</button>
